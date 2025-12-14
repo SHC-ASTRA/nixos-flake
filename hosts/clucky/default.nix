ac0a2085b0e10b666d538ad89f3b9ff1c0c36609
SHC-ASTRA <90978381+ASTRA-SHC@users.noreply.github.com>
2025-12-13 20:56:53 -0600

    got nvidia working with docker

--- hosts/clucky/default.nix
+++ hosts/clucky/default.nix
@@ -5,12 +5,49 @@
     inputs.hardware.nixosModules.common-gpu-nvidia-nonprime
   ];
 
-  hardware.nvidia.open = false;
+  nixpkgs.config.allowUnfree = true;
 
-  hardware.nvidia-container-toolkit.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.open = false;
+  hardware.nvidia-container-toolkit.enable = true;
+  
+
+  # Docker & nvidia Runtime config
+  virtualisation.docker.enable = true;
+
+  virtualisation.docker.daemon.settings = {
+    runtimes = {
+      nvidia = {
+        path = "${pkgs.nvidia-container-toolkit}/bin/nvidia-container-runtime";
+        runtimeArgs = [];
+      };
+    };
+
+    # Enable CDI support
+    features = {
+      cdi = true;
+    };
+  };
+
+  # Ensure /etc/cdi exists
+  systemd.tmpfiles.rules = [
+    "d /etc/cdi 0755 root root -"
+  ];
+
+  systemd.services.nvidia-cdi-generator = {
+    description = "Generate NVIDIA CDI spec for containers";
+    wantedBy = [ "multi-user.target" ];
+    before = [ "docker.service" ];
+    after = [ "systemd-modules-load.service" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.nvidia-container-toolkit}/bin/nvidia-ctk cdi generate --output=/etc/cdi/nvidia.yaml";
+    };
+  };
 
   environment.systemPackages = with pkgs; [
-	xorg.xhost
+	  xorg.xhost
+    nvidia-container-toolkit
   ];
 }
