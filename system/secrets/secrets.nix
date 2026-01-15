c50b42f126a7a4c0847dadbbe830d77de5d12ce6
ryleu <69326171+ryleu@users.noreply.github.com>
2026-01-15 04:45:51 -0600

    rekey for new clucky installation

--- system/secrets/secrets.nix
+++ system/secrets/secrets.nix
@@ -2,7 +2,7 @@ let
   users = import ./authorized_keys.nix;
 
   testbed = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPk+0RwYnmHx5jl/b+/jqiGO5l4tNNtPTElXpYsmVbnl root@testbed";
-  clucky = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPvjS8AeOg270Y00imwSky4oo+7S7c00O3L1COqZQn6m root@clucky";
+  clucky = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID3uLg1EJtz9lOvdT6LplKL8mUEckXOaW7RGC3ND+qi3 root@clucky";
   deck = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAPGoyey9zJE+hON+W79+lSPIzDjWdeNXG7iXzZP/N0u root@deck";
 
   systems = [
