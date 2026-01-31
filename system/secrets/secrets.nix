5b77d151c66adcb030dc7b725d3a04c1a45930c9
ryleu <69326171+ryleu@users.noreply.github.com>
2026-01-31 02:09:01 -0600

    rekey for panda

--- system/secrets/secrets.nix
+++ system/secrets/secrets.nix
@@ -1,14 +1,16 @@
 let
   users = import ./authorized_keys.nix;
 
-  testbed = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPk+0RwYnmHx5jl/b+/jqiGO5l4tNNtPTElXpYsmVbnl root@testbed";
   clucky = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID3uLg1EJtz9lOvdT6LplKL8mUEckXOaW7RGC3ND+qi3 root@clucky";
   deck = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAPGoyey9zJE+hON+W79+lSPIzDjWdeNXG7iXzZP/N0u root@deck";
+  panda = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKW2tl2+6PL1QHRgF6CISeIHcF9cHMzOEFpaPmJ1OlgQ root@panda";
+  testbed = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPk+0RwYnmHx5jl/b+/jqiGO5l4tNNtPTElXpYsmVbnl root@testbed";
 
   systems = [
-    testbed
     clucky
     deck
+    panda
+    testbed
   ];
 in
 {
