736d279f5231f8b0b6152eab3132d269f879bb0f
SHC-ASTRA <90978381+ASTRA-SHC@users.noreply.github.com>
2026-02-04 01:37:26 -0600

    tested

--- home/bash.nix
+++ home/bash.nix
@@ -11,7 +11,7 @@
         neofetch = "fastfetch";
       };
       profileExtra = ''
-        [[ -e ~/.ssh/id_ed25519 ]] && ssh-add ~/.ssh/id_ed25519
+        [[ -e ~/.ssh/id_ed25519 ]] && ssh-add ~/.ssh/id_ed25519 &> /dev/null
       '';
     };
 
