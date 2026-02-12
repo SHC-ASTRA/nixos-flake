cc768b9580abaf3caf663b14147175495112d6ad
Riley M. <69326171+ryleu@users.noreply.github.com>
2026-02-12 02:11:43 -0600

    Remove rtl88xxau-aircrack from boot.extraModulePackages
    Removed rtl88xxau-aircrack from extra module packages.

--- system/default.nix
+++ system/default.nix
@@ -8,8 +8,4 @@
   ]
   ++ (if host.isGraphical then [ ./graphical ./non-graphical ] else [ ./non-graphical ])
   ++ (if host.isNvidia then [ ./nvidia ] else []);
-
-  boot.extraModulePackages = [
-    config.boot.kernelPackages.rtl88xxau-aircrack
-  ];
 }
