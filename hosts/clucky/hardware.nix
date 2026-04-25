85edeab5e6fa3ffe63dac0797fbe8f4450b8f413
SHC-ASTRA <90978381+ASTRA-SHC@users.noreply.github.com>
2026-04-25 15:00:50 -0500

    feat: clucky is now on a sata SSD

--- hosts/clucky/hardware.nix
+++ hosts/clucky/hardware.nix
@@ -18,37 +18,36 @@
     "xhci_pci"
     "thunderbolt"
     "ahci"
-    "nvme"
     "usbhid"
+    "usb_storage"
+    "sd_mod"
   ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/fbdd4788-1171-4a2f-a926-b699bd63cd70";
-    fsType = "ext4";
+    device = "/dev/disk/by-label/NIXROOT";
+    fsType = "btrfs";
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/5048-0244";
+    device = "/dev/disk/by-label/NIXBOOT";
     fsType = "vfat";
     options = [
-      "fmask=0077"
-      "dmask=0077"
+      "fmask=0022"
+      "dmask=0022"
     ];
   };
 
-  swapDevices = [
-    { device = "/dev/disk/by-uuid/93950c04-89d5-4b0d-a37e-259b2590a9bc"; }
-  ];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp86s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp85s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
