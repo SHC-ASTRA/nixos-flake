ebef4d12710cff30a21d724c6f9be15114699015
John-Thomas-Cook <tcjohn05@gmail.com>
2026-05-02 13:42:25 -0500

    Sourced the command

--- system/non-graphical/autostart.nix
+++ system/non-graphical/autostart.nix
@@ -63,7 +63,7 @@
         wantedBy = [ "default.target" ];
 
         serviceConfig = {
-          ExecStart = "ros2 launch realsense2_camera rs_launch.py enable_rgbd:=true enable_sync:=true align_depth.enable:=true enable_color:=true enable_depth:=true";
+          ExecStart = "/bin/sh -c '. /etc/profile; ros2 launch realsense2_camera rs_launch.py enable_rgbd:=true enable_sync:=true align_depth.enable:=true enable_color:=true enable_depth:=true'";
           Restart = "always";
           RestartSec = 5;
           Environment = ''
