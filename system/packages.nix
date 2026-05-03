700d14611006e95b01986020c0fc00437edba83f
ryleu <69326171+ryleu@users.noreply.github.com>
2026-05-03 17:20:24 -0500

    feat: add some qol programs

--- system/packages.nix
+++ system/packages.nix
@@ -1,32 +1,43 @@
 { pkgs, inputs, ... }:
 {
-  environment.systemPackages =
-    with pkgs;
-    [
-      # System
-      gh
-      socat
-      usbutils
-      silver-searcher
-      wl-clipboard
+  environment = {
+    systemPackages =
+      with pkgs;
+      [
+        # System
+        gh
+        socat
+        usbutils
+        silver-searcher
+        wl-clipboard
+        btop
+        tree
+        ripgrep
+        comma
+        bat
 
-      # Programming
-      micro
-      nil
-      nixd
-      neovim
-      ripgrep
-      nixfmt-rfc-style
-      tmux
+        # Programming
+        micro
+        nil
+        nixd
+        neovim
+        ripgrep
+        nixfmt-rfc-style
+        tmux
 
-      # Build stuff
-      gcc
-      colcon
-      gnumake
-      python312Packages.pyserial
-    ]
-    ++ (with inputs.basestation-cameras.packages.${pkgs.system}; [
-      cameracli
-      pkgs.parallel # i don't know how to fix this
-    ]);
+        # Build stuff
+        gcc
+        colcon
+        gnumake
+        python312Packages.pyserial
+      ]
+      ++ (with inputs.basestation-cameras.packages.${pkgs.system}; [
+        cameracli
+        pkgs.parallel # i don't know how to fix this
+      ]);
+
+    shellAliases = {
+      vim = "nvim";
+    };
+  };
 }
