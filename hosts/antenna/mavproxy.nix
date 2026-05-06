65f0d8af55c34b93f9a823fdb2fc14d3482a2016
ryleu <69326171+ryleu@users.noreply.github.com>
2026-05-06 16:26:55 -0500

    fix(mavproxy): issue with quoting

--- hosts/antenna/mavproxy.nix
+++ hosts/antenna/mavproxy.nix
@@ -1,27 +1,24 @@
 { pkgs, ... }:
 let
   device = "/dev/serial/by-id/usb-STMicroelectronics_STM32_Virtual_ComPort_206438A93430-if00";
+
+  start-mavproxy = pkgs.writeShellScript "start-mavproxy" ''
+    set -eu
+    while [ ! -e ${device} ]; do
+      sleep 10
+    done
+    export PYTHONPATH="${pkgs.python312Packages.future}/${pkgs.python312.sitePackages}:''${PYTHONPATH:-}"
+    exec ${pkgs.mavproxy}/bin/mavproxy.py --master=${device}
+  '';
 in
 {
   systemd.user.services.mavproxy = {
     enable = true;
-    after = [ "default.target" ];
-    requires = [ "default.target" ];
     wantedBy = [ "default.target" ];
-
     serviceConfig = {
-      ExecStart = ''
-        bash -c "
-                while [[ ! -e ${device} ]]; do
-        	  sleep 10
-        	done
-
-                export PYTHONPATH="${pkgs.python312Packages.future}/${pkgs.python312.sitePackages}:''$PYTHONPATH"
-        	${pkgs.mavproxy}/bin/mavproxy.py
-              "'';
+      ExecStart = "${start-mavproxy}";
       Restart = "always";
       RestartSec = 10;
-
     };
   };
 }
