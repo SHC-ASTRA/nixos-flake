e77cc0aca3f906d90cb92f180ec38a492656f73b
ryleu <69326171+ryleu@users.noreply.github.com>
2026-05-09 13:46:54 -0400

    refactor(mavproxy): use ExecStartPre

--- hosts/antenna/mavproxy.nix
+++ hosts/antenna/mavproxy.nix
@@ -2,11 +2,15 @@
 let
   device = "/dev/serial/by-id/usb-STMicroelectronics_STM32_Virtual_ComPort_206438A93430-if00";
 
-  start-mavproxy = pkgs.writeShellScript "start-mavproxy" ''
+  mavproxy-device-check = pkgs.writeShellScript "mavproxy-device-check" ''
     set -eu
-    while [ ! -e ${device} ]; do
+    while [[ ! -e ${device} ]]; do
       sleep 10
     done
+  '';
+
+  start-mavproxy = pkgs.writeShellScript "start-mavproxy" ''
+    set -eu
     export PYTHONPATH="${pkgs.python312Packages.future}/${pkgs.python312.sitePackages}:''${PYTHONPATH:-}"
     exec ${pkgs.mavproxy}/bin/mavproxy.py --master=${device}
   '';
@@ -16,6 +20,7 @@ in
     enable = true;
     wantedBy = [ "default.target" ];
     serviceConfig = {
+      ExecStartPre = "${mavproxy-device-check}";
       ExecStart = "${start-mavproxy}";
       Restart = "always";
       RestartSec = 10;
