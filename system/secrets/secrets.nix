f8db152eefbcc7f1a6437c351c86ca92686bbf44
ryleu <69326171+ryleu@users.noreply.github.com>
2026-05-26 01:59:45 -0600

    feat: reinstall clucky

--- system/secrets/secrets.nix
+++ system/secrets/secrets.nix
@@ -2,7 +2,7 @@ let
   users = import ./authorized_keys.nix;
 
   antenna = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIwsUyh1bedH185l55BuQCo3Ag98ltApYV8yh7Y3uW21 root@antenna";
-  clucky = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID3uLg1EJtz9lOvdT6LplKL8mUEckXOaW7RGC3ND+qi3 root@clucky";
+  clucky = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGld2cFIuIgcehMLL86vheR8w0zO32H/EAHt4WqFpxDr root@clucky";
   deck = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAPGoyey9zJE+hON+W79+lSPIzDjWdeNXG7iXzZP/N0u root@deck";
   panda = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKW2tl2+6PL1QHRgF6CISeIHcF9cHMzOEFpaPmJ1OlgQ root@panda";
   testbed = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPk+0RwYnmHx5jl/b+/jqiGO5l4tNNtPTElXpYsmVbnl root@testbed";
