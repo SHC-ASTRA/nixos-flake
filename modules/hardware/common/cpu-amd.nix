8ed4ef9974a1e57953eef9b2b9d6b8990772b267
ryleu <69326171+ryleu@users.noreply.github.com>
2026-06-16 18:38:34 -0500

    feat(hardware): ensure graphics are available n cpu-*.nix

--- modules/hardware/common/cpu-amd.nix
+++ modules/hardware/common/cpu-amd.nix
@@ -1,5 +1,18 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   boot.kernelModules = [ "kvm-amd" ];
+  boot.initrd.kernelModules = [ "amdgpu" ];
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      amdvlk
+      rocmPackages.clr.icd
+    ];
+  };
 }
--- modules/hardware/common/cpu-intel.nix
+++ modules/hardware/common/cpu-intel.nix
@@ -1,5 +1,18 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   boot.kernelModules = [ "kvm-intel" ];
+  boot.initrd.kernelModules = [ "i915" ];
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      vpl-gpu-rt
+    ];
+  };
 }
