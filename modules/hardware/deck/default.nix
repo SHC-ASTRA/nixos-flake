b5a1550873684e552e8b47f0f5b7e1f06157d885
ryleu <69326171+ryleu@users.noreply.github.com>
2026-06-17 17:46:32 -0500

    feat(deck): reorient bootloader (thanks roald)

--- modules/hardware/deck/default.nix
+++ modules/hardware/deck/default.nix
@@ -9,5 +9,9 @@
   networking.hostName = "deck";
   astra.role.basestation.enable = true;
 
+  # provides Steam Deck controller drivers and on-screen keyboard
   programs.steam.enable = true;
+
+  # sets the bootloader to horizontal (Steam Decks have weird screens)
+  boot.loader.systemd-boot.consoleMode = "5";
 }
